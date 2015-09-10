6e8179a9020438e67fad2c2fff55676777a54f7d
Masatoshi Nishimura <massa_n@yahoo.ca>
2015-09-10 14:39:15 -0400

    navbar popup

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,37 @@
-angular.module('App', []);
+angular.module('App', [])
+    .controller('MainController', function($scope, $timeout) {
+        var win = $(window);
+        var overHeroPos = 522;
+
+        win.scroll(function() {
+
+            if (win.scrollTop() > 522) {
+                $timeout(function() {
+                    $scope.showNav = true;
+                }, 10);
+
+            } else {
+            	$timeout(function() {
+                    $scope.showNav = false;
+                }, 10);
+            }
+
+            console.log("You've scrolled " + win.scrollTop() + " pixels");
+        });
+    })
+    .controller('ServiceController', function($scope) {
+        $scope.services = [{
+            title: 'Strategy',
+            contents: ['Digital Strategy', 'Enterprise Application', 'Strategy UX Strategy', 'Strategic Innovation', 'Mobile Strategy', 'Digital Transformation']
+        }, {
+            title: 'Design',
+            contents: ['User Experience Design', 'Mobile & Tablet Design', 'User Interface Design', 'Motion Graphics & Animation', 'Usability Testing & User Research']
+        }, {
+            title: 'Technology',
+            contents: ['Web Application', 'Mobile & Tablet Applications', 'E-commerce & M-commerce', 'Content Management Systems (CMS) Implementation', 'System Architecture & Enterprise Integration', 'Prototyping & Product Development']
+        }, {
+            title: 'Marketing',
+            contents: ['Brand Strategy', 'Identity Design', 'Customer Engagement Strategy', 'Marketing Automation', 'Media Production and Planning', 'Brochure & Collateral Production']
+        }]
+    })
+  
